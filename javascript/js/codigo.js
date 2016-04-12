35f09e6b53c334f8656f3beaab50e9749debdc66
echagaray <chael_35hotmail.com>
2016-04-12 10:59:43 -0600

    archivo js,funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,11 @@
+
+	var botonClic =function()
+	{
+		alert("Clic del boton")
+			}
+	var nombre="Michael";
+document.write("<strong>JS en accion </strong>");+" " +nombre;
+alert("Mensaje por pantalla ");
+console.log(" A punto de lanzar un mensaje");
+alert("Mensaje por pantalla")
+alert("Hola mundo");
