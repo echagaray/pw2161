3eb4f69f56a0fc9872524a3cd835368ff4951305
echagaray <chael_35hotmail.com>
2016-04-18 11:04:56 -0600

    Gato no terminado

---
+++ javascript/js/gato.js
@@ -0,0 +1,37 @@
+var operador ="";
+
+
+function numeros(num)
+{
+
+
+
+
+if 	
+
+
+
+
+
+
+
+
+	//Escribir en el operando1
+	var valor=document.gato
+	if (operador=="") //vaciamos la caja
+
+	{
+		if (valor=="X")	//concatenar los valores de num con los del operando1
+			document.calculadora.operando1.value =
+			document.calculadora.operando1.value + num;
+		
+		else {
+			if (valor=="O")	//concatenar los valores de num con los del operando1
+			document.calculadora.operando2.value =
+			document.calculadora.operando2value + num;
+		}
+			
+	}
+
+
+}
