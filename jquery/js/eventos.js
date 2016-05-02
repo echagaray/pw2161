79e2725abf5f69dc67b5417c9a300457ead839d8
echagaray <chael_35hotmail.com>
2016-05-02 11:00:50 -0600

    jquery focus

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -4,14 +4,41 @@ var inicio = function()
 var clicBoton= function()
 {
 	console.log("clic del boton");
-	$(".anuncioWeb").html("Clic del boton")
+	//$(".anuncioWeb").html("Clic del boton")
 	$(".anuncioWeb").append("Clic del boton")
 }
 
 //preparar los eventos 
 //de todos mis objetos
-$("#miBoton").on("click",clicBoton);
+//	º	$("#miBoton").on("click",clicBoton);
 }
 //main
 
 $(document).on("ready,inicio");
+
+var clicBoton2= function()
+{
+	alert("boton 2");
+}	
+
+var clicBoton2= function(tecla)
+{
+	if (tecla.which == 13)
+	{
+$("·#otroInput").focus():
+
+	}
+}
+	$(".anuncioWeb").off("click","Clic del boton")
+	$(".anuncioWeb").on("click","Clic del boton2")
+	$("#unInput").on("keypress",teclaUnInput);
+
+
+//preparar los eventos 
+//de todos mis objetos
+
+
+$(document).off("ready,inicio");
+
+
+
