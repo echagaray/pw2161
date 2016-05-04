8ffcfc83568b795bdbc13dfe2f44b2b851a339b2
echagaray <chael_35hotmail.com>
2016-05-04 11:04:58 -0600

    jquery consigue imagen

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -5,60 +5,64 @@ var clicBoton= function()
 {
 	console.log("clic del boton");
 	//$(".anuncioWeb").html("Clic del boton")
-	$(".anuncioWeb").append("Clic del boton")
+	$(".anuncioWeb").append("Clic del boton");
 }
 
 //preparar los eventos 
 //de todos mis objetos
 //	º	$("#miBoton").on("click",clicBoton);
-}
+
 //main
 
-$(document).on("ready,inicio");
+
 
 var clicBoton2= function()
 {
-	beforeSend:function(){
+	
+	$.ajax({
+		beforeSend:function(){
 console.log("Espere..");
 	},
-	$.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data){
   console.log(data);
-  alert(data.results[0].name.first+
-  	data.results[0].name.last);
-
+  //alert(data.results[0].name.first+data.results[0].name.last);
+  //mostramos la informacion en html
+$("#fotoPersona").attr("src",data.results[0].picture.medium);
+$("#txtNombreUser").html("src",data.results[0].name.first);
+$("#txtApellidoUser").html("src",data.results[0].name.last);
 
 },
 error: function(xhr,error,throws){
 console.log("ocurrio un error");
 
-}
-  	);
+
+  	
   }
 });
      
 }	
 
-var clicBoton2= function(tecla)
+var clicBBoton2= function(tecla)
 {
 	if (tecla.which == 13)
 	{
-$("·#otroInput").focus():
+$("·#otroInput").focus();
 
 	}
 }
-	$(".anuncioWeb").off("click","Clic del boton")
-	$(".anuncioWeb").on("click","Clic del boton2")
-	$("#unInput").on("keypress",teclaUnInput);
+	$(".anuncioWeb").off("click",clicBoton2)
+	$(".anuncioWeb").on("click",clicBoton2)
+	$("#miBoton").on("click",clicBoton2)
+	//$("#unInput").on("keypress",teclaUnInput);
 
 
 //preparar los eventos 
 //de todos mis objetos
 
-
-$(document).off("ready,inicio");
+}
+$(document).on("ready",inicio);
 
 
 
